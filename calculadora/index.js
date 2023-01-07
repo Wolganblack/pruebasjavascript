7b6b889df376edd5763c9693147afc690bcb5965
Wolganblack <ALFONSO@DESKTOP-7VNR7GU>
2023-01-06 22:19:10 -0600

    Mi segundo commit

---
+++ calculadora/index.js
@@ -0,0 +1,51 @@
+/*console.log("Script conectado exitosamente");*/
+
+const txtop1 = document.getElementById("operador1");
+const txtoperador = document.getElementById("operador");
+const txtoperador2 = document.getElementById("operador2");
+const btncalculador = document.getElementById("calcular");
+const txtresultado = document.getElementById("resultado");
+const presultado = document.getElementById("Resultado1");
+
+/*btncalculador.addEventListener("click", calcular);*/
+
+function calcula() {
+    /* console.log("Apretaste el Boton Calcular");*/
+    const operador = txtoperador.value;
+    const op1 = parseFloat(txtop1.value);
+    const op2 = parseFloat(txtoperador2.value);
+
+    if (
+        operador == "+" ||
+        operador == "-" ||
+        operador == "/" ||
+        operador == "*"
+    ) {
+
+        let resultadofinal;
+        switch (operador) {
+            case "+":
+                resultadofinal = op1 + op2
+                break;
+
+            case "-":
+                resultadofinal = op1 - op2
+                break;
+
+            case "*":
+                resultadofinal = op1 * op2
+                break;
+
+            case "/":
+                resultadofinal = op1 / op2
+                break;
+
+        }
+        presultado.innerText = "=" + resultadofinal;
+        txtresuldado.innerText = "=" + resultadofinal;
+    } else {
+        txtresuldado1.innerText = "Calculo ImPosible";
+        txtresuldado.innerText = "Calculo ImPosible";
+    }
+    return;
+}
